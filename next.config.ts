f6629f4bece9ac816e682a48dad729f26477129a
Joshtri <stuffofyos1516@gmail.com>
2025-12-07 14:29:59 +0700

    Commit: next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,23 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+    ],
+    domains: ["localhost", "127.0.0.1", "::1"],
+  },
   reactStrictMode: false,
   typescript: {
     ignoreBuildErrors: true,
+  },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "50mb",
+    },
   },
 };
 
