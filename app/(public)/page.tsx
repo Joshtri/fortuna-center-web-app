77c5eb1f53c1f2c1a0b78e351ccc75f56a499761
Joshtri <stuffofyos1516@gmail.com>
2025-12-07 14:29:36 +0700

    Commit: page.tsx

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -1,10 +1,10 @@
 "use client";
 
-import HeroSection from "@/features/landing-page/HeroSection";
-import AboutSection from "@/features/landing-page/AboutSection";
-import ProgramsSection from "@/features/landing-page/ProgramsSection";
-import GallerySection from "@/features/landing-page/GallerySection";
-import LocationSection from "@/features/landing-page/LocationSection";
+import HeroSection from "@/features/public/landing-page/HeroSection";
+import AboutSection from "@/features/public/landing-page/AboutSection";
+import ProgramsSection from "@/features/public/landing-page/ProgramsSection";
+import GallerySection from "@/features/public/landing-page/GallerySection";
+import LocationSection from "@/features/public/landing-page/LocationSection";
 import ScrollToTopButton from "@/components/ui/ScrollToTopButton";
 
 export default function LandingPage() {
